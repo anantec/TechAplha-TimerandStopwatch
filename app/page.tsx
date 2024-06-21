3ab9433b28908e30b572af7c946775c55b90cece
Anant Singh Dugtal <93126578+anantec@users.noreply.github.com>
2024-06-21 10:18:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Stopwatch from "../src/stopwatch";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-    <h1 className="text-4xl font-bold mb-4">Timer and Stopwatch Example</h1>
+    <h1 className="text-4xl font-bold mb-4">Timer and Stopwatch</h1>
     <Timer initialTime={30} />
     <Stopwatch initialTime={0} />
   </div>
